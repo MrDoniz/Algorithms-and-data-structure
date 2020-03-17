6180d05eabd07d796de9c64b68fddc3e72174778
Daniel Dóniz García <33846493+MrDoniz@users.noreply.github.com>
2020-03-17 20:59:12 +0000

    test

--- Second practice/matrix_t.hpp
+++ Second practice/matrix_t.hpp
@@ -149,11 +149,11 @@ template<class T> void matrix_t<T>::multiply(const matrix_t<T>& A, const matrix_
   
   for(int i = 1; i <= m_; i++)
     for(int j = 1; j <= n_; j++){
-			T aux;
-			for(int k = 1; k <= A.getN(); k++){
-				aux = aux + (A.at(i, k) * B.at(k, j));
-				at(i, j) = aux;
+      T aux;
+      for(int k = 1; k <= A.getN(); k++){
+        aux = aux + (A.at(i, k) * B.at(k, j));
+        at(i, j) = aux;
       }
       aux = 0;
-		}
+    }
 }
